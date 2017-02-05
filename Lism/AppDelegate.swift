658356ad76e7827c0a7a6c53ee48c9a8715bad86
nofelmahmood <nofelmehmood@gmail.com>
2017-02-05 19:59:49 +0500

    Show Login page by default

--- Lism/AppDelegate.swift
+++ Lism/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.rootViewController = ViewController();
+        self.window?.rootViewController = LoginViewController();
         self.window?.backgroundColor = UIColor.white
         self.window?.makeKeyAndVisible()
         

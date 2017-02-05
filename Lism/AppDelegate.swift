ee02cc551f055fe5d82aedce5b24130114de422c
nofelmahmood <nofelmehmood@gmail.com>
2017-02-05 20:15:47 +0500

    Add navigation bar

--- Lism/AppDelegate.swift
+++ Lism/AppDelegate.swift
@@ -17,10 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        let navigationController = UINavigationController()
         let mainViewController = MainViewController()
+        navigationController.viewControllers = [mainViewController]
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.rootViewController = mainViewController
+        self.window?.rootViewController = navigationController
         self.window?.backgroundColor = UIColor.white
         self.window?.makeKeyAndVisible()
         

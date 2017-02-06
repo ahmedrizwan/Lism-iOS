8d33bc891066a542ade4b1681297b20012d77b0c
nofelmahmood <nofelmehmood@gmail.com>
2017-02-06 18:53:50 +0500

    Convert all view elements to lazy props

--- Lism/Login/LoginViewController.swift
+++ Lism/Login/LoginViewController.swift
@@ -9,63 +9,83 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    lazy var logoImageView: UIImageView = {
+        let image = UIImage()
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        let imageViewHeightConstraint = NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 50.0)
+        imageView.addConstraints([imageViewHeightConstraint])
+        
+        return UIImageView(image: image)
+    }()
+    
+    lazy var usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Phone/Email/Username"
+        textField.autocapitalizationType = .none
+        
+        return textField
+    }()
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Password"
+        textField.isSecureTextEntry = true
+        
+        return textField
+    }()
+    
+    
+    lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Login", for: .normal)
+        button.backgroundColor = UIColor.black
+        
+        return button
+    }()
+    lazy var forgotPasswordButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Forgot Password", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        
+        return button
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let sV = UIStackView(arrangedSubviews: [self.logoImageView, self.usernameTextField, self.passwordTextField, self.loginButton, self.forgotPasswordButton])
+        sV.translatesAutoresizingMaskIntoConstraints = false
+        sV.axis = .vertical
+        sV.distribution = .fill
+        sV.spacing = 16.0
+        
+        return sV
+    }()
+    
+    lazy var centerXStackView: NSLayoutConstraint = {
+        let constraint = NSLayoutConstraint(item: self.stackView, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1.0, constant: 0.0)
+        
+        return constraint
+    }()
+    lazy var centerYStackView: NSLayoutConstraint = {
+        let constraint = NSLayoutConstraint(item: self.stackView, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1.0, constant: 0.0)
+        
+        return constraint
+    }()
 
-    var stackView: UIStackView!
-    
-    var logoImageView: UIImageView!
-    
-    var usernameTextField: UITextField!
-    var passwordTextField: UITextField!
-    
-    var loginButton: UIButton!
-    var forgotPasswordButton: UIButton!
-    
-    var centerYStackView: NSLayoutConstraint!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.white
-
         // Do any additional setup after loading the view.
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
-        
-        usernameTextField = UITextField()
-        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
-        usernameTextField.placeholder = "Phone/Email/Username"
-        usernameTextField.autocapitalizationType = .none
-        
-        passwordTextField = UITextField()
-        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
-        passwordTextField.placeholder = "Password"
-        passwordTextField.isSecureTextEntry = true
-        
-        loginButton = UIButton()
-        loginButton.translatesAutoresizingMaskIntoConstraints = false
-        loginButton.setTitle("Login", for: .normal)
-        loginButton.backgroundColor = UIColor.black
-        
-        forgotPasswordButton = UIButton()
-        forgotPasswordButton.translatesAutoresizingMaskIntoConstraints = false
-        forgotPasswordButton.setTitle("Forgot Password", for: .normal)
-        forgotPasswordButton.setTitleColor(UIColor.blue, for: .normal)
-        
-        let image = UIImage()
-        logoImageView = UIImageView(image: image)
-        logoImageView.translatesAutoresizingMaskIntoConstraints = false
-        let imageViewHeightConstraint = NSLayoutConstraint(item: logoImageView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 50.0)
-        logoImageView.addConstraints([imageViewHeightConstraint])
-        
-        stackView = UIStackView(arrangedSubviews: [logoImageView, usernameTextField, passwordTextField, loginButton, forgotPasswordButton])
-        stackView.axis = .vertical
-        stackView.distribution = .fill
-        stackView.spacing = 16.0
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
+
+        view.backgroundColor = UIColor.white
+
         view.addSubview(stackView)
-        let centerXStackView = NSLayoutConstraint(item: stackView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0.0)
-        centerYStackView = NSLayoutConstraint(item: stackView, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1.0, constant: 0.0)
         view.addConstraints([centerXStackView, centerYStackView])
     }
 

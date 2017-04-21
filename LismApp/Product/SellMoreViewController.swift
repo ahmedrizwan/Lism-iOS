276f106149e51b6de002159c3c2999688bea9cc1
laraibmalik <mehabi101@gmail.com>
2017-04-22 00:49:15 +0500

    completed UI of update page

--- LismApp/Product/SellMoreViewController.swift
+++ LismApp/Product/SellMoreViewController.swift
@@ -2,7 +2,7 @@
 import Foundation
 import AVOSCloud
 
-class SellMoreViewController: UIViewController,UIImagePickerControllerDelegate,    UINavigationControllerDelegate,UITableViewDelegate, UITableViewDataSource 
+class SellMoreViewController: UIViewController,UIImagePickerControllerDelegate,    UINavigationControllerDelegate,UITableViewDelegate, UITableViewDataSource, UITextViewDelegate
 {
     
     @IBOutlet weak var colorsTableView : UITableView!
@@ -12,7 +12,9 @@ class SellMoreViewController: UIViewController,UIImagePickerControllerDelegate, 
 
     @IBOutlet weak var selectedCategoryBtn : UIButton!
     @IBOutlet weak var sizesBtn : UIButton!
+    @IBOutlet weak var sizesBtnHeightConstaint : NSLayoutConstraint!
 
+    
     @IBOutlet weak var addCamBtn1 : UIButton!
     @IBOutlet weak var addCamBtn2 : UIButton!
     @IBOutlet weak var addCamBtn3 : UIButton!
@@ -25,6 +27,22 @@ class SellMoreViewController: UIViewController,UIImagePickerControllerDelegate, 
     @IBOutlet weak var crossBtn5 : UIButton!
     
     @IBOutlet weak var colorsBtn : UIButton!
+    
+      @IBOutlet weak var productNameTextfield: UITextField!
+    @IBOutlet weak var brandTextfield: UITextField!
+
+    @IBOutlet weak var estimatedPriceLabel : UILabel!
+    @IBOutlet weak var estimatedTextField: UITextField!
+
+    @IBOutlet weak var sellingPriceLabel : UILabel!
+    @IBOutlet weak var sellingPriceTextField: UITextField!
+    @IBOutlet weak var itemsConditionBtn : UIButton!
+    @IBOutlet weak var itemConditionLabel : UILabel!
+
+    @IBOutlet weak var postForSaleBtn : UIButton!
+     @IBOutlet weak var descTextView : UITextView!
+
+    let PLACEHOLDER_TEXT = "PRODUCT DESCRIPTION"
 
     var selectedBtn = UIButton()
      var btnToUpdateText = UIButton()
@@ -38,11 +56,16 @@ class SellMoreViewController: UIViewController,UIImagePickerControllerDelegate, 
      var isShoesOrClothing = false
     
     var brands :[AVObject] = []
-    var itemConditions :[AVObject] = []
+    var itemConditions : [String: Any]!
+    var itemConditionsToRender : [String: Any]!
+
 
     let picker = UIImagePickerController()
     var colorsDictionary  : [String: String]!
     
+    var arrayOFEnabledButtons = [UIButton]()
+    var allButtons = [UIButton]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         picker.delegate = self
@@ -53,6 +76,33 @@ class SellMoreViewController: UIViewController,UIImagePickerControllerDelegate, 
         loadInfo() {
             print("Background Fetch Complete")
         }
+        allButtons.append(addCamBtn1)
+        allButtons.append(addCamBtn2)
+        allButtons.append(addCamBtn3)
+        allButtons.append(addCamBtn4)
+        allButtons.append(addCamBtn5)
+        self.setUpDescriptionTextView()
+
+    }
+    func setUpDescriptionTextView()
+    {
+        sizesBtnHeightConstaint.constant = 0
+        productNameTextfield.layer.borderWidth = 1.0
+        brandTextfield.layer.borderWidth = 1.0
+        sellingPriceTextField.layer.borderWidth = 1.0
+        estimatedTextField.layer.borderWidth = 1.0
+
+        descTextView.layer.borderColor = UIColor.gray.cgColor
+        productNameTextfield.layer.borderColor = UIColor.gray.cgColor
+        brandTextfield.layer.borderColor = UIColor.gray.cgColor
+        sellingPriceTextField.layer.borderColor = UIColor.gray.cgColor
+        estimatedTextField.layer.borderColor = UIColor.gray.cgColor
+
+
+        descTextView.layer.borderWidth = 1.0
+        descTextView.delegate = self
+        applyPlaceholderStyle(aTextview: descTextView, placeholderText: PLACEHOLDER_TEXT)
+        
     }
     func loadInfo(completionHandler: (() -> Void)!) {
         self.loadBrandsInfo()
@@ -88,17 +138,17 @@ class SellMoreViewController: UIViewController,UIImagePickerControllerDelegate, 
     }
     @IBAction func crossButton2Action(sender : AnyObject)
     {
-        self.clearImage(imageToClear: addCamBtn2, imageToDisable : crossBtn2)
+        self.clearImage(imageToClear: addCamBtn2, imageToDisable : crossBtn2 )
  
     }
     @IBAction func crossButton3Action(sender : AnyObject)
     {
-        self.clearImage(imageToClear: addCamBtn3, imageToDisable : crossBtn3)
+        self.clearImage(imageToClear: addCamBtn3, imageToDisable : crossBtn3 )
  
     }
     @IBAction func crossButton4Action(sender : AnyObject)
     {
-        self.clearImage(imageToClear: addCamBtn4, imageToDisable : crossBtn4)
+        self.clearImage(imageToClear: addCamBtn4, imageToDisable : crossBtn4 )
    
     }
     @IBAction func crossButton5Action(sender : AnyObject)
@@ -106,44 +156,77 @@ class SellMoreViewController: UIViewController,UIImagePickerControllerDelegate, 
         self.clearImage(imageToClear: addCamBtn5, imageToDisable : crossBtn5)
 
     }
-    func clearImage(imageToClear : UIButton ,imageToDisable : UIButton)
+    func clearImage(imageToClear : UIButton ,imageToDisable : UIButton )
     {
         imageToDisable.isHidden = true
-        
-        imageToClear.setBackgroundImage(UIImage(named :"camera"), for: .normal)
+        imageToClear.isUserInteractionEnabled = true
+        let indexOfButton = arrayOFEnabledButtons.index(of: imageToClear)
+        if(indexOfButton != nil)
+        {
+        arrayOFEnabledButtons.remove(at: indexOfButton!)
+        }
+        let buttonToAdd = updateImageIfNotset()
+        buttonToAdd.setBackgroundImage(UIImage(named :"addPhotoAsset 1"), for: .normal)
+       //set all others as cam
+        self.setAllOtherCamExcept(buttonWithAdd: buttonToAdd)
+        // imageToClear.setBackgroundImage(UIImage(named :"camera"), for: .normal)
     }
-    func updateImageIfNotset()
-    {
     
+    func setAllOtherCamExcept(buttonWithAdd : UIButton)
+    {
+        
+        for button in allButtons
+        {
+            if(button != buttonWithAdd && !arrayOFEnabledButtons.contains(button))
+            {
+                button.setBackgroundImage(UIImage(named :"camera"), for: .normal)
+            }
+        }
     }
+
+    
+    func updateImageIfNotset() -> UIButton
+    {
+        
+        for button in allButtons
+        {
+        if(!arrayOFEnabledButtons.contains(button))
+        {
+          return button
+        }
+        }
+        return UIButton()
+}
     
     @IBAction func camera1AddButtonAction(sender : AnyObject)
     {
-        self.addCameraAction(selectedBtn: addCamBtn1, imageToEnable: crossBtn1, nextButtonToAdd : addCamBtn2)
+        self.addCameraAction(selectedBtn: addCamBtn1, imageToEnable: crossBtn1, nextButtonToAdd : updateImageIfNotset())
     }
     @IBAction func camera2AddButtonAction(sender : AnyObject)
     {
-        self.addCameraAction(selectedBtn: addCamBtn2, imageToEnable: crossBtn2 , nextButtonToAdd : addCamBtn3)
+        self.addCameraAction(selectedBtn: addCamBtn2, imageToEnable: crossBtn2 , nextButtonToAdd : updateImageIfNotset())
 
     }
     @IBAction func camera3AddButtonAction(sender : AnyObject)
     {
-        self.addCameraAction(selectedBtn: addCamBtn3, imageToEnable: crossBtn3 , nextButtonToAdd : addCamBtn4)
+        self.addCameraAction(selectedBtn: addCamBtn3, imageToEnable: crossBtn3 , nextButtonToAdd : updateImageIfNotset())
    
     }
     @IBAction func camera4AddButtonAction(sender : AnyObject)
     {
-        self.addCameraAction(selectedBtn: addCamBtn4, imageToEnable: crossBtn4 , nextButtonToAdd : addCamBtn5)
+        self.addCameraAction(selectedBtn: addCamBtn4, imageToEnable: crossBtn4 , nextButtonToAdd : updateImageIfNotset())
   
     }
     @IBAction func camera5AddButtonAction(sender : AnyObject)
     {
-        self.addCameraAction(selectedBtn: addCamBtn5, imageToEnable: crossBtn5, nextButtonToAdd : UIButton())
+        self.addCameraAction(selectedBtn: addCamBtn5, imageToEnable: crossBtn5, nextButtonToAdd : updateImageIfNotset())
 
     }
     func addCameraAction(selectedBtn: UIButton, imageToEnable : UIButton  , nextButtonToAdd : UIButton)
     {
         self.selectedBtn = selectedBtn
+         self.selectedBtn.isUserInteractionEnabled = false
+        arrayOFEnabledButtons.append(self.selectedBtn)
         self.crossBtnToEnable = imageToEnable
         self.nextBtnToEnable = nextButtonToAdd
         picker.allowsEditing = true
@@ -170,6 +253,78 @@ class SellMoreViewController: UIViewController,UIImagePickerControllerDelegate, 
         dismiss(animated:true, completion: nil) //5
     }
     
+    func applyPlaceholderStyle(aTextview: UITextView, placeholderText: String)
+    {
+        // make it look (initially) like a placeholder
+        aTextview.textColor = UIColor.lightGray
+        aTextview.text = placeholderText
+    }
+    
+    func applyNonPlaceholderStyle(aTextview: UITextView)
+    {
+        // make it look like normal text instead of a placeholder
+        aTextview.textColor = UIColor.darkText
+        aTextview.alpha = 1.0
+        
+    }
+    func textViewShouldBeginEditing(aTextView: UITextView) -> Bool
+    {
+        if aTextView == descTextView && aTextView.text == PLACEHOLDER_TEXT
+        {
+            // move cursor to start
+            moveCursorToStart(aTextView: aTextView)
+        }
+        return true
+    }
+    func moveCursorToStart(aTextView: UITextView)
+    {
+        aTextView.selectedRange = NSMakeRange(0, 0);
+
+    }
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+       
+        let newLength = textView.text.utf16.count + text.utf16.count - range.length
+        if newLength > 0 // have text, so don't show the placeholder
+        {
+            // check if the only text is the placeholder and remove it if needed
+            // unless they've hit the delete button with the placeholder displayed
+            if textView == descTextView && textView.text == PLACEHOLDER_TEXT
+            {
+                if text.utf16.count == 0 // they hit the back button
+                {
+                    return false // ignore it
+                }
+                applyNonPlaceholderStyle(aTextview: textView)
+                textView.text = ""
+            }
+            return true
+        }
+        else  // no text, so show the placeholder
+        {
+            applyPlaceholderStyle(aTextview: textView, placeholderText: PLACEHOLDER_TEXT)
+            moveCursorToStart(aTextView: textView)
+            return false
+        }
+    }
+    
+    @IBAction func itemCoditionButtonAction(sender : AnyObject)
+    {
+        btnToUpdateText = sender as! UIButton
+         let dict = itemConditions as NSDictionary
+         if(self.selectedCategoryBtn.titleLabel?.text == "Shoes" || self.selectedCategoryBtn.titleLabel?.text == "Handbags")
+         {
+           
+            self.itemConditionsToRender = dict.value(forKey: "Shoes and Handbags") as! [String: Any]
+  
+        }
+        else
+         {
+            self.itemConditionsToRender = dict.value(forKey: "Clothing And Accessories") as! [String: Any]
+
+        }
+              self.showAlertcontrollerForITemsCondtion(title: "Item Condition" , objectToDisplay : self.itemConditionsToRender)
+}
+    
     @IBAction func categoryButtonAction(sender : AnyObject)
     {
         btnToUpdateText = sender as! UIButton
@@ -185,6 +340,10 @@ class SellMoreViewController: UIViewController,UIImagePickerControllerDelegate, 
     {
         colorsTableViewParent.isHidden = false;
         colorsTableView.reloadData()
+    }
+    @IBAction func  postForSaleAction(sender : AnyObject)
+    {
+    
     }
     func loadCategoriesInfo()
     {
@@ -256,8 +415,12 @@ class SellMoreViewController: UIViewController,UIImagePickerControllerDelegate, 
         query.findObjectsInBackground { (objects, error) in
             if(error == nil)
             {
-                self.itemConditions = objects as! [AVObject]
+                
+                self.itemConditions = self.convertToDictionary(text:(objects?[0] as! AVObject).value(forKey: "value") as! String)!
+                
+            
             }
+            
             
             
         }
@@ -280,16 +443,19 @@ class SellMoreViewController: UIViewController,UIImagePickerControllerDelegate, 
             
     
             self.btnToUpdateText.setTitle(result.title!, for: .normal)
-            if(self.selectedCategoryBtn.titleLabel?.text == "Shoes" || self.selectedCategoryBtn.titleLabel?.text == "Clothing")
+            self.btnToUpdateText.titleLabel?.text = result.title!
+            if(self.selectedCategoryBtn.titleLabel?.text == "Shoes" || self.selectedCategoryBtn.titleLabel?.text == "Clothing" || !self.sizesBtn.isHidden)
             {
-                
+                self.sizesBtnHeightConstaint.constant = self.selectedCategoryBtn.frame.size.height
                 self.sizesBtn.isHidden = false
                 
             }
             else
             {
+                self.sizesBtnHeightConstaint.constant = 0
                 self.sizesBtn.isHidden = true
             }
+            self.view.updateConstraintsIfNeeded()
             self.showAlertcontrollerArray(title: "\(key)" , objectToDisplay: value as! [Any])
             print(result.title!)
         }
@@ -298,6 +464,35 @@ class SellMoreViewController: UIViewController,UIImagePickerControllerDelegate, 
     }
     alertController.addAction(cancelAction)
     self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
+    func showAlertcontrollerForITemsCondtion(title: String, objectToDisplay : [String: Any])
+    {
+        
+        
+        
+        let alertController = UIAlertController(title: title, message:"", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.view.tintColor = UIColor.darkGray
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) { (result : UIAlertAction) -> Void in
+        }
+        for (key, value) in objectToDisplay {
+            print("key: \(value)")
+            let okAction = UIAlertAction(title: "\(key)", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
+                
+                
+                
+                self.btnToUpdateText.setTitle(key , for: .normal)
+          
+                print(result.title!)
+            }
+            alertController.addAction(okAction)
+            
+        }
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: nil)
     }
     
     
